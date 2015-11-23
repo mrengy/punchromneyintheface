b277be3ffdbf018f4cbbbf4e9e8d18065e8e07e3
Mike Eng <mrengy@gmail.com>
2015-11-23 08:51:31 -0500

    hardcoded width and height of eel

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
 	var spotCounter = 1;
+	var eelWidth=127;
+	var eelHeight=127;
     $('#face-container').mousedown(function(event){
     	$(this).addClass('punching');
-    	$(this).append($('#original-spot').clone().removeAttr('id').attr('id','spot-'+spotCounter).css('left', event.pageX).css('top', event.pageY));
+    	$(this).append($('#original-spot').clone().removeAttr('id').attr('id','spot-'+spotCounter).css('left', event.pageX - eelWidth).css('top', event.pageY - eelHeight));
     	spotCounter++;
     });
     $('#face-container').mouseup(function(){
