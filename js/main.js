bb57d91fc5e69d4745dcab184b02585246daae6c
Mike Eng <mrengy@gmail.com>
2015-11-23 20:42:04 -0500

    sound working. prototype complete for Chrome desktop

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $( document ).ready(function() {
-	var spotCounter = 1;
+	$('audio#eel').trigger('load');
+    var spotCounter = 1;
 	var eelWidth=127;
 	var eelHeight=127;
+
     $('#face-container').mousedown(function(event){
     	$(this).addClass('punching');
     	$(this).append($('#original-spot').clone().removeAttr('id').attr('id','spot-'+spotCounter).css('left', event.pageX - eelWidth).css('top', event.pageY - eelHeight));
-    	spotCounter++;
+    	$('audio#eel').trigger('play');
+        spotCounter++;
     });
     $('#face-container').mouseup(function(){
     	$(this).removeClass('punching');
